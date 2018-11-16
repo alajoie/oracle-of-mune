5d96912d830a6d338ae64d019ad9d5127f061c85
alajoie <alexis.lajoie@gmail.com>
2018-11-15 19:37:24 -0600

    extracted method to force decisive answers

--- src/acolyte.py
+++ src/acolyte.py
@@ -18,20 +18,24 @@ class Acolyte:
             self.assertive_counter = 0
         if self.get_assertive_counter() > 2:
             self.assertive_counter = 0
-            if self.isPositive(answer):
-                return "Yes."
-            else:
-                return "No."
+            return self.forceDecisiveAnswer(answer)
         else:
             index = answer - 1
             return self.translations[index]
-    def get_assertive_counter(self):
-        return self.assertive_counter
 
     def isIndecisive(self, answer):
         if answer != 2 or answer != 5:
             return True
         return False
+
+    def get_assertive_counter(self):
+        return self.assertive_counter
+
+    def forceDecisiveAnswer(self, answer):
+        if self.isPositive(answer):
+            return "Yes."
+        else:
+            return "No."
 
     def isPositive(self, answer):
         if answer >= 4 and answer < 7:
